19b841416f30b7c57813b27325f6eec8e7f7bb5b
Michael Sutanto <michaelu@ecogreenoleo.co.id>
2026-05-19 23:56:40 +0800

    chore: remove unnecessary folders, keep backend and frontend only

--- storage/framework/views/65f6a5c386ba7b24a09baf24ce96466b.php
+++
@@ -1,9 +0,0 @@
- <footer class="main-footer">
- 	<div class="footer-left">
- 		Copyright &copy; 2024 <div class="bullet"></div> Created by AnD Department <div class="bullet"></div> <a href="http://www.ecogreenoleo.com/">PT. Ecogreen Oleochemicals</a>
- 	</div>
- 	<div class="footer-right">
-
- 	</div>
- </footer>
-<?php /**PATH C:\XAMPP\htdocs\EODS\Master\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
